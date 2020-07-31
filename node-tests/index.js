373799a811ba54ce9e83eae24554f691ef545051
Sean Purcell <me@seanp.xyz>
2020-07-31 00:48:13 -0400

    Add some other message types to node test

--- node-tests/index.js
+++ node-tests/index.js
@@ -3,8 +3,10 @@ var io = require('socket.io')();
 var port = Number(process.argv[2]) || 0;
 
 io.on('connect', socket => {
-    console.log('connection received with id ' + socket.id);
+    console.error('connection received with id ' + socket.id);
     socket.emit('test', 'hello', {'key': 'value'});
+    socket.binary(true).emit('binary', Buffer.from([0xde, 0xad, 0xbe, 0xef]), Buffer.from([0xde, 0xad, 0xbe, 0xef]));
+    socket.binary(false).emit('binary', Buffer.from([0xde, 0xad, 0xbe, 0xef]));
 });
 
 io.attach(port);
