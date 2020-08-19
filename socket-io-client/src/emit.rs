80aaf2730af5f2dd3d975eddd589fe24d12b0284
Sean Purcell <me@seanp.xyz>
2020-08-19 13:34:57 -0400

    Add AckBuilder struct

--- socket-io-client/src/emit.rs
+++ socket-io-client/src/emit.rs
@@ -1,3 +1,5 @@
+use async_tungstenite::tungstenite::Message as WsMessage;
+use futures::channel::mpsc;
 use serde::Serialize;
 
 use socket_io_protocol::socket::PacketBuilder;
@@ -16,6 +18,18 @@ pub struct EventArgsBuilder<'a> {
     client: &'a mut Client,
     namespace: &'a str,
     callback: Option<(AckCallback, u64)>,
+    builder: PacketBuilder,
+}
+
+pub struct AckBuilder {
+    send: mpsc::UnboundedSender<Vec<WsMessage>>,
+    namespace: String,
+    id: u64,
+    binary: bool,
+}
+
+pub struct AckArgsBuilder {
+    send: mpsc::UnboundedSender<Vec<WsMessage>>,
     builder: PacketBuilder,
 }
 
@@ -77,5 +91,48 @@ impl<'a> EventArgsBuilder<'a> {
                 .set_ack(self.namespace, id, callback);
         }
         let _ = self.client.send.unbounded_send(packets); // TODO: Determine if we care about the result.
+    }
+}
+
+impl AckBuilder {
+    pub(crate) fn new(
+        send: mpsc::UnboundedSender<Vec<WsMessage>>,
+        namespace: impl Into<String>,
+        id: u64,
+    ) -> Self {
+        AckBuilder {
+            send,
+            namespace: namespace.into(),
+            id,
+            binary: false,
+        }
+    }
+
+    pub fn binary(&mut self, b: bool) -> &mut Self {
+        self.binary = b;
+        self
+    }
+
+    pub fn args(self) -> AckArgsBuilder {
+        let builder = PacketBuilder::new_ack(self.namespace, self.id, self.binary);
+        AckArgsBuilder {
+            send: self.send,
+            builder,
+        }
+    }
+}
+
+impl AckArgsBuilder {
+    pub fn arg<T>(&mut self, arg: &T) -> Result<&mut Self, ArgsError>
+    where
+        T: Serialize + ?Sized,
+    {
+        self.builder.serialize_arg(arg)?;
+        Ok(self)
+    }
+
+    pub fn finish(self) {
+        let packets = self.builder.finish();
+        let _ = self.send.unbounded_send(packets); // TODO: Determine if we care about the result.
     }
 }
